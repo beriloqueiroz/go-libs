a71abe5278930f4d7f375b49a8c2f484e365ed32
berilo queiroz <luiz.berilo@buson.com.br>
2024-02-20 22:21:46 -0300

    cmd project

---
+++ cmd/go.mod
@@ -0,0 +1,3 @@
+module github.com/beriloqueiroz/my-go-libs/cmd
+
+go 1.21.4
---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/beriloqueiroz/my-go-libs/validation"
+)
+
+func main() {
+	fmt.Println(validation.Cpf("123654878"))
+}
